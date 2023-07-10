4d304350e9dd97b14887f1c6247bbe64e00e9830
rohina usman <rohina.usman@gmail.com>
2023-07-10 14:58:52 +0000

    dataeventlistview dataed to views.py

--- eventbooking/views.py
+++ eventbooking/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, get_object_or_404, redirect
+from django.views import generic, View
 
 # Create your views here.
+
+
+class EventList(generic.ListView):
+    """
+    events overview and pagination
+    """
+    model = Event
+    queryset = Event.objects.filter(status=1).order_by('-created_on')
+    template_name = 'event.html'
+    paginate_by = 6
