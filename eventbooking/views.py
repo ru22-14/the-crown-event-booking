db232c4605084914b890397e37f5c3839479e766
rohina usman <rohina.usman@gmail.com>
2023-07-12 23:14:19 +0000

    event details file created and data added to it

--- eventbooking/views.py
+++ eventbooking/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, reverse, get_object_or_404, redirect
 from django.views import generic, View
+from django.views.generic import TemplateView
 from .models import Event
 from .models import Booking
 
@@ -14,13 +15,58 @@ class EventList(generic.ListView):
     queryset = Event.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+    
 
+class EventDetail(View):
 
-class EventDetails(View):
-    """
-    events overview and pagination
-    """
-    model = Event
-    queryset = Event.objects.filter(status=1).order_by('-created_on')
-    template_name = 'event.details.html'
-    paginate_by = 6         
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "events.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+    
+    def post(self, request, slug, *args, **kwargs):
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "events.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form,
+                "liked": liked
+            },
+        )
+       
