f1c68810eea239ccf5d2e6f8a98c5ba47e93af85
rohina usman <rohina.usman@gmail.com>
2023-07-12 16:26:11 +0000

    event details added to template

--- eventbooking/urls.py
+++ eventbooking/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.EventList.as_view(), name='home'),
     path('all_events/', views.EventList.as_view(), name='all_events'),
+    path('events/', views.EventDetails.as_view(), name='events'),
 ]
--- eventbooking/views.py
+++ eventbooking/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, reverse, get_object_or_404, redirect
 from django.views import generic, View
 from .models import Event
+from .models import Booking
 
 # Create your views here.
 
@@ -13,3 +14,13 @@ class EventList(generic.ListView):
     queryset = Event.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class EventDetails(View):
+    """
+    events overview and pagination
+    """
+    model = Event
+    queryset = Event.objects.filter(status=1).order_by('-created_on')
+    template_name = 'event.details.html'
+    paginate_by = 6         
