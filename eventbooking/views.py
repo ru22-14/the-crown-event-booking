c9472a6599e789e2e417fc3a487b413b96324d42
rohina usman <rohina.usman@gmail.com>
2023-07-17 16:59:42 +0000

    form added to booking app, data added to form. booking.html updated

--- eventbooking/views.py
+++ eventbooking/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views import generic, View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 from .models import Event, Booking
+from .forms import BookingForm
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.urls import reverse_lazy
 import datetime
 
 
@@ -22,6 +26,33 @@ class EventListView(generic.ListView):
     paginate_by = 6
 
 
-class EventBookingView(TemplateView):
+# class EventBooking(CreateView):
+#     model = Booking
+#     template_name = 'booking.html'
+    
+#     def booking(self, request):
+#         submitted = False
+#         if request.method == "POST":
+#             form = BookingForm(request.POST)
+#             if form.is_valid():
+#                 booking.save()
+#                 messages.success(request, 'you booked successfully')
+#                 return HttpResponseRedirect('/booking?submitted=True')
+#         else:
+#             form = BookingForm
+#             if 'submitted' in request.GET:
+#                 submitted = True        
+#         return render(request, 'booking.html', {'form': form, 'submitted': submitted, })
+class EventBookingCreateView(CreateView):
     model = Booking
+    form_class = BookingForm
     template_name = 'booking.html'
+    success_url = reverse_lazy('booking')  # URL to redirect after successful form submission
+
+    def form_valid(self, form):
+        if form.is_valid():
+            response = super().form_valid(form)
+            messages.success(self.request, 'You booked successfully')
+            return response  
+        else:
+            return messages.error(self.request, 'something went wrong')
