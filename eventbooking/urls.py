f759dc1c7de04644fa65ab42c090db71ebd37dd9
rohina usman <rohina.usman@gmail.com>
2023-07-20 22:44:46 +0000

    comment and like option added to event page

--- eventbooking/urls.py
+++ eventbooking/urls.py
@@ -1,11 +1,12 @@
 from . import views
 from django.urls import path
-from .views import EventPageView, EventListView
+from .views import EventPageView, EventListView, EventReviews, EventLike
 
 
 urlpatterns = [
     path('', views.EventPageView.as_view(), name='home'),
     path('events/', views.EventListView.as_view(), name='events'),
-    path('events/', views.EventReviews.as_view(), name='events'),
+    path('comment/<slug:slug>', views.EventReviews.as_view(), name='event_comment'),
+    # path('like/<slug:slug>', views.EventLike.as_view(), name='events'),
     
 ] 
--- eventbooking/views.py
+++ eventbooking/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views import generic, View
 from django.views.generic import TemplateView, CreateView
-from .models import Event
+from .models import Event, Comment
 from .forms import CommentForm
 from django.http import HttpResponseRedirect
 from django.contrib import messages
@@ -48,6 +48,48 @@ class EventReviews(generic.ListView):
             },
         ) 
 
+    def post(self, request, slug, *args, **kwargs):
 
+        queryset = Event.objects.filter(status=1)
+        event = get_object_or_404(queryset, slug=slug)
+        comments = event.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if event.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.useremail
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+           
+            comment.save()
+            messages.success(request, 'Thank You For your Review!')
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "events.html",
+            {
+                "event": event,
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form,
+                "liked": liked
+            },
+        )    
+
+
+class EventLike(View):
+    
+    def post(self, request, slug, *args, **kwargs):
+        event = get_object_or_404(Event, slug=slug)
+        if event.likes.filter(id=request.user.id).exists():
+            event.likes.remove(request.user)
+        else:
+            event.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('events', args=[slug]))
 
    
