653aebc9f8c64b8d567bc739c69199ba80566bfe
rohina usman <rohina.usman@gmail.com>
2023-07-29 15:01:27 +0000

    views tests created

--- eventbooking/test_forms.py
+++ eventbooking/test_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .forms import CommentForm, BookingForm
+from datetime import date
 
 
 class TestCommentForm(TestCase):
@@ -29,4 +30,4 @@ class TestBookingForm(TestCase):
         
     def test_fields_are_explicit_in_forms_metaclass(self):
         form = BookingForm()
-        self.assertEqual(form.meta.fields, ['event', 'guests', 'date', 'timeblock', 'menu', 'drinks','cake', 'theme', 'username'])    
+        self.assertEqual(form.Meta.fields, ['event', 'guests', 'date', 'timeblock', 'menu', 'drinks','cake', 'theme', 'username'])    
--- eventbooking/test_views.py
+++ eventbooking/test_views.py
@@ -1,9 +1,62 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Event, Booking, Comment
+from django.contrib.auth.models import User
+from datetime import datetime, date
+from .views import (
+    EventListView,
+    MyBookingView,
+    EventBookingView,
+    UpdateBookingView,
+    DeleteBookingView,
+)
 
-# Create your tests here.
-# class TestDjango(TestCase):
-#     def testing(self):
-#         self.assertEqual(1, 0)
 
-#      def testing(self):
-#         self.assertEqual(1, 2)    
+class TestViews(TestCase):
+    
+    @classmethod
+    def setUpTestData(self):
+        self.client = Client()
+        # self.user = User.objects.create_user(
+        #     username='rumaissa',
+        #     password='1482'
+        # )
+
+        self.event = Event.objects.create(
+            title='test event party',
+            slug='test-event-slug-party',
+            description='this is the test event datenight content',
+            status=1
+        )
+        self.comment = Comment.objects.create(
+            event=self.event,
+            username=self.user,
+            message='this is a comment for event party'
+        )
+
+        self.booking = Booking.objects.create(
+
+            username=self.user,
+            event='Eid Party',
+            guests=40,
+            date='July 31, 2023',
+            time='10 AM - 12 AM',
+            menu='oriental',
+            drinks='juices',
+            theme='oriental',
+            cake='no',
+            approved=False,
+        )
+
+    def test_get_home_page(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'base.html')
+        self.assertTemplateUsed(response, 'index.html')
+        
+
+
+        
+        
+    
+
+       
