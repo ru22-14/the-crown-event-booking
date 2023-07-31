09b4da29a0d9b6b59f8d20290eff58ebf7f7704b
rohina usman <rohina.usman@gmail.com>
2023-07-31 09:21:56 +0000

    debug set to false

--- thecrownevent/settings.py
+++ thecrownevent/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-ru2214-thecrowneventboo-y2chvgkpkub.ws-eu102.gitpod.io', 'localhost', 'the-crown-event-d0c891afb09a.herokuapp.com']
 
